e48e2e7d455f3da5ef823a0af6de0927ac15a8e1
Mimu-salon <tmimura0@gmail.com>
2021-05-26 23:11:09 +0900

    ⚰️ Init

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,21 +1,14 @@
-import { extendTheme } from '@chakra-ui/react'
-import { createBreakpoints } from '@chakra-ui/theme-tools'
-
-const fonts = { mono: `'Menlo', monospace` }
-
-const breakpoints = createBreakpoints({
-  sm: '40em',
-  md: '52em',
-  lg: '64em',
-  xl: '80em',
-})
+import { extendTheme } from '@chakra-ui/react';
 
 const theme = extendTheme({
-  colors: {
-    black: '#16161D',
+  styles: {
+    global: {
+      body: {
+        backgroundColor: 'gray.100',
+        color: 'gray.800',
+      },
+    },
   },
-  fonts,
-  breakpoints,
-})
+});
 
-export default theme
+export default theme;
